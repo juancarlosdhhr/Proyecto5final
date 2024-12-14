472362edc5ed538b1c0c79d3ab61c5c8825a51aa
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-12-14 15:10:05 +0000

    update

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -3,20 +3,23 @@
 import './Header.css';
 
 const template = () => `
-  <header>
-  <section>
-          <div class="header-content">
-            <img src="https://static.vecteezy.com/system/resources/previews/020/500/024/non_2x/cupra-logo-brand-car-symbol-with-name-white-design-spanish-automobile-illustration-with-black-background-free-vector.jpg" alt="Cupra Logo" class="logo" />
-          </div>
-          <div>
-            <nav class="nav-buttons">
-              <button class="nav-button">Ofertas</button>
-              <button class="nav-button">Configurador</button>
-              <button class="nav-button">Cookies</button>
-              <button class="nav-button"><i class="fa fa-user"></i></button>
-            </nav>
-          </div>
-        </header>
+  <header class="header">
+    <div class="header-content">
+      <img 
+        src="https://static.vecteezy.com/system/resources/previews/020/500/024/non_2x/cupra-logo-brand-car-symbol-with-name-white-design-spanish-automobile-illustration-with-black-background-free-vector.jpg" 
+        alt="Cupra Logo" 
+        class="logo" 
+      />
+      <nav class="nav-buttons">
+        <button class="nav-button">Ofertas</button>
+        <button class="nav-button">Configurador</button>
+        <button class="nav-button">Cookies</button>
+        <button class="nav-button">
+          <i class="fa fa-user"></i>
+        </button>
+      </nav>
+    </div>
+  </header>
 `;
 
 const Header = () => {
--- components/Novedades/Novedades.js
+++ components/Novedades/Novedades.js
@@ -6,17 +6,17 @@ const template = () => `
     <h2>Novedades</h2>
     <div class="novedades-container">
       <div class="novedad-card">
-        <img src="path_to_image1.jpg" alt="CUPRA Terramar" class="novedad-image" />
+        <img src="https://www.cupraofficial.es/content/dam/countries/es/cupra-website/new-home/three-cards-highlights/x-large/cupra-terramar.jpg" alt="CUPRA Tavascán" class="novedad-image" alt="CUPRA Terramar" class="novedad-image" />
         <h3>Nuevo CUPRA Terramar</h3>
         <p>Un SUV híbrido sumamente emocional y electrificado...</p>
       </div>
       <div class="novedad-card">
-        <img src="path_to_image2.jpg" alt="CUPRA Tavascán" class="novedad-image" />
+        <img src="https://www.cupraofficial.es/content/dam/countries/es/cupra-website/new-home/three-cards-highlights/x-large/cupra-tavascan.jpg" />
         <h3>Nuevo CUPRA Tavascán</h3>
         <p>Nuestro primer SUV coupé eléctrico.</p>
       </div>
       <div class="novedad-card">
-        <img src="path_to_image3.jpg" alt="CUPRA León" class="novedad-image" />
+        <img src="https://www.cupraofficial.es/content/dam/countries/es/cupra-website/new-home/three-cards-highlights/x-large/leon-cupra.jpg" alt="CUPRA León" class="novedad-image" />
         <h3>Nuevo CUPRA León</h3>
         <p>Un hilo rojo que conecta con nuestro nuevo modelo.</p>
       </div>
