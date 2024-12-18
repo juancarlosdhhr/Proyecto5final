ba5961ad2eef777c3d1db87a32d16d0cb7d9d414
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-12-18 02:26:51 +0000

    update

--- components/Slider/Slider.js
+++ components/Slider/Slider.js
@@ -6,21 +6,21 @@ const template = () => `
     <div class="slider-wrapper">
       <div class="slider-container">
         <div class="slider-card">
-          <img src="public/slider1.jpg" alt="vista de pinos" class="slider-image" />
+          <img src="../../assets/slider1.jpg" alt="vista de pinos" class="slider-image" />
           <div class="slider-text-right">
             <h3>Energía más limpia</h3>
             <p>Reduce tu huella de carbono.</p>
           </div>
         </div>
         <div class="slider-card">
-          <img src="public/slider2.jpg" alt="Imagen aparcamientos" class="slider-image" />
+          <img src="../../assets/slider2.jpg" alt="Imagen aparcamientos" class="slider-image" />
           <div class="slider-text-right">
             <h3>Ahorra sobre la marcha</h3>
             <p>Disfruta de descuentos en aparcamiento y peajes. Ahorra más gracias al Plan MOVES y a la deducción fiscal sobre el IRPF.</p>
           </div>
         </div>
         <div class="slider-card">
-          <img src="public/slider3.jpg" alt="Imagen de taller mecanico" class="slider-image" />
+          <img src="../../assets/slider3.jpg" alt="Imagen de taller mecanico" class="slider-image" />
           <div class="slider-text-right">
             <h3>Menor mantenimiento</h3>
             <p>En comparación con los vehículos de combustión interna tradicionales, los vehículos eléctricos requieren menos mantenimiento.</p>
