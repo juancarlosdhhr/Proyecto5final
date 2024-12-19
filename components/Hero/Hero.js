117fd9874a298ff27144c63856f2e21ad7acb5ad
juancarlosdhhr <juancarlosdhhr@gmail.com>
2024-12-19 20:49:40 +0000

    update

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -4,21 +4,20 @@ import './Hero.css';
 const Hero = () => {
   const heroContainer = document.getElementById('hero-container');
 
- 
+  // Asegúrate de que heroContainer existe antes de continuar
   if (!heroContainer) {
     console.error('El contenedor del Hero no existe.');
     return;
   }
 
-
+  // Asignar el HTML como una cadena con autoplay y muted
   heroContainer.innerHTML = `
     <section class="hero">
-      <video id="hero-video" autoplay loop>
+      <video id="hero-video" autoplay loop muted controls>
         <source src="../../assets/Mercedes-Benz VISION AVTR.mp4" type="video/mp4">
         Tu navegador no soporta el video.
       </video>
-      <button id="mute-button" class="mute-button">🔊</button>
-      <div class="gradient-overlay"></div>
+      
       <div class="content">
         <h1>NUEVO MERCEDES AVATAR DESDE 99.900€¹</h1>
         <p>Movimiento inteligente, belleza extraordinaria</p>
@@ -28,26 +27,6 @@ const Hero = () => {
       </div>
     </section>
   `;
-
-  const video = document.getElementById('hero-video');
-  const muteButton = document.getElementById('mute-button');
-
- 
-  console.log('Botón de mute encontrado:', muteButton);
-  console.log('Video encontrado:', video);
-
-  
-  video.muted = false;
-  video.play().catch(() => {
-    video.muted = true;
-    muteButton.textContent = '🔇';
-  });
-
-  
-  muteButton.addEventListener('click', () => {
-    video.muted = !video.muted;
-    muteButton.textContent = video.muted ? '🔇' : '🔊';
-  });
 };
 
 export default Hero;
